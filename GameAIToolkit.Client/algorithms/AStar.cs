716bab41d0ec404fa891dfeb38741f82f4b5b111
Aishakhan17 <aishakhan_19@hotmail.com>
2026-09-13 00:32:25 +0100

    Initial commit with gitignore

---
+++ GameAIToolkit.Client/algorithms/AStar.cs
@@ -0,0 +1,10 @@
+namespace GameAIToolkit.Client.algorithms
+{
+    class AStarAlgorithm
+    {
+        public void Write()
+        {
+            Console.WriteLine("A* algorithm file");
+        }
+    }
+}
